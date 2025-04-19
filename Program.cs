a7ffad74df35e62913f8fe126db67892d5caacf8
andybykov <123543480+andybykov@users.noreply.github.com>
2025-04-19 21:10:04 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿/* Упражнение 5 */
+/* Упражнение 5 */
 
 /* 
 Требуется создать программу, которая подсчитывает площадь 
@@ -6,29 +6,17 @@
 */
 using System; // подключение пространства имен System
 
-class AreaTriangle
+class AreaOfTriangle
 {
     static void Main(string[] args)
     {
-
-        int a, b, c;                                          // объявляем переменные
-        Console.WriteLine("Enter side a:");                    //приглашение к вводу
-        a = Int32.Parse(Console.ReadLine());                  //инициализируем а
-
-        Console.WriteLine("Enter side b:");                    //приглашение к вводу
-        b = Int32.Parse(Console.ReadLine());                  //инициализируем b
-
-        Console.WriteLine("Enter side c:");                    //приглашение к вводу
-        c = Int32.Parse(Console.ReadLine());                  //инициализируем c
-
-
-        double perimeter = (double) (a + b + c);                      // рассчитываем периметир
-        double p = perimeter / 2;                          // рассчитываем полупериметр - явно преобразуем в double
-
-        //Console.WriteLine("Semi-perimerter = {0:F3}", p); // форматируем до 3 знаков после запятой
-        double area = Math.Sqrt(p * ((p - a) * (p - b) * (p - c)));
-        Console.WriteLine("Area of triangle = {0:F3}", area); // форматируем до 3 знаков после запятой
-
+        Console.WriteLine("Enter perimeter of triangle:");      //приглашение к вводу
+        double perimeter = Double.Parse(Console.ReadLine());    //получаем периметр
+        double side = perimeter / 3;                            //расситываем сторону
+        double seimPer = perimeter / 2;                         // рассчитываем полупериметер
+       // Console.WriteLine("Semi-perimerter = {0:F3}", seimPer); // форматируем до 3 знаков после запятой
+        double area = Math.Sqrt(seimPer * Math.Pow(seimPer - side, 3));  //вычисляем площадь для равностороннего треугольника
+        Console.WriteLine("Area of triangle = {0:F3}", area); // форматируем до 3 знаков после запятой 
 
     }
 }
